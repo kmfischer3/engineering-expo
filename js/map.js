b5f13f02dc23e5293864e73e7e0e4b4d1b6b6b98
Austin Hartzheim <inbox@austinhartzheim.me>
2017-04-02 19:58:07 +0000

    Flattened maps.js and updated code for easier debugging. Updated gulpfile.js to allow for Python map analysis code (coming soon). Relocated maps files to static/maps/.

--- js/map.js
+++ js/map.js
@@ -1,55 +1,57 @@
 var map = {
-    MIN_TABLE_NUMBER: 1,
-    MAX_TABLE_NUMBER: 117,  // TODO: update
+    MIN_TABLE_NUMBER: 0,
+    MAX_TABLE_NUMBER: 87  // TODO: update
+};
 
-    resetTables: function(day) {
-	      for (i = map.MIN_TABLE_NUMBER; i <= map.MAX_TABLE_NUMBER; i++) {
-	          var table = document.getElementById("table" + day.toString() + "." + i.toString());
-	          if (table == null)
-		            continue;
+map.resetTables = function(day) {
+	for (i = map.MIN_TABLE_NUMBER; i <= map.MAX_TABLE_NUMBER; i++) {
+	    var table = document.getElementById("table" + i.toString());
+        console.assert(table != null, "Table with id=%i does not exist.", i);
+	    table.classList.remove("highlight");
+	}
 
-	          table.classList.remove("highlight");
-	      }
+    // TODO: update to hide companies across all days
+};
 
-        // TODO: update to hide companies across all days
-    },
+map.highlightTables = function(day, table_ids) {
+    table_ids.forEach(function(table_id) {
+	    var table = document.getElementById("table" + table_id.toString());
+        console.assert(table != null, "Table with id=%i does not exist.", table_id);
+	    table.classList.add("highlight");
+    });
+};
 
-    highlightTables: function(day, table_ids) {
-        table_ids.forEach(function(table_id) {
-	        var table = document.getElementById("table" + day.toString() + "." + table_id.toString());
-	        table.classList.add("highlight");
-        });
-    },
+/*
+ * options = {
+ *   company_ids: [1,2,3],
+ *   day: 0
+ * }
+ */
+map.highlightCompanies = function(options) {
+    map.resetTables(options.day);
 
-    /*
-     * options = {
-     *   company_ids: [1,2,3],
-     *   day: 0
-     * }
-     */
-    highlightCompanies: function(options) {
-        map.resetTables(options.day);
+    options.company_ids.forEach(function(company_id, index, array) {
+        var company = data[company_id];
+        map.highlightTables(options.day, company.tables_on_day(options.day));
+    });
+};
 
-        options.company_ids.forEach(function(company_id, index, array) {
-            var company = data[company_id];
-            map.highlightTables(options.day, company.tables_on_day(options.day));
-        });
-    },
+/*
+ * Load the map files in the background.
+ */
+map.loadMaps = function() {
+    $("#map_svg_0").load("/static/maps/square_map0.svg");
+    $("#map_svg_1").load("/static/maps/square_map1.svg");
+    $("#map_svg_2").load("/static/maps/square_map2.svg");
+    $("#map_svg_3").load("/static/maps/square_map3.svg");
+    $("#map_svg_4").load("/static/maps/square_map4.svg");
+    $("#map_svg_5").load("/static/maps/square_map5.svg");
+};
 
-    /*
-     * Load the map files in the background.
-     */
-    loadMaps: function() {
-        $("#map_svg_0").load("/static/map-2016-09-19.svg");
-        $("#map_svg_1").load("/static/map-2016-09-21.svg");
-        $("#map_svg_2").load("/static/map-2016-09-28.svg");
-    },
-
-    /*
-     * Show the correct map for a given day, hiding the others.
-     */
-    showMap: function(day) {
-        $(".map_svg").addClass("hidden");
-        $("#map_svg_" + day.toString()).removeClass("hidden");
-    }
+/*
+ * Show the correct map for a given day, hiding the others.
+ */
+map.showMap = function(day) {
+    $(".map_svg").addClass("hidden");
+    $("#map_svg_" + day.toString()).removeClass("hidden");
 };
